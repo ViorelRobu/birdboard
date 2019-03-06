0ddee6508ccf9bd6c5803ad8612b0885ed2acfe7
Viorel Robu <wwjd.vio@gmail.com>
2019-03-06 22:00:26 +0200

    Finished TTD'ing the main Project MVC

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -6,27 +6,32 @@ use App\Project;
 
 class ProjectsController extends Controller
 {
-
     public function index()
     {
-        $projects = Project::all();
+        $projects = auth()->user()->projects;
         return view('projects.index', compact('projects'));
     }
 
+    public function create()
+    {
+        return view('projects.create');
+    }
     public function store()
     {
         $attributes = request()->validate([
             'title'=>'required',
-            'description'=>'required'
+            'description'=>'required',
             ]);
 
-        Project::create($attributes);
+        auth()->user()->projects()->create($attributes);
 
         return redirect('/projects');
     }
 
     public function show(Project $project)
     {
+        abort_if(auth()->user()->isNot($project->owner), 403);
+
         return view('projects.show', compact('project'));
     }
 }
--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -6,13 +6,30 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ProjectsTest extends TestCase
+class ManageProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
+
+    /** @test */
+    public function guests_cannot_manage_projects()
+    {
+        $project = factory('App\Project')->create();
+
+        $this->get('/projects')->assertRedirect('/login');
+        $this->get('/projects/create')->assertRedirect('/login');
+        $this->post('/projects', $project->toArray())->assertRedirect('/login');
+        $this->get($project->path())->assertRedirect('/login');
+    }
+
     /** @test */
     public function a_user_can_create_a_project()
     {
         $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\User')->create());
+
+        $this->get('/projects/create')->assertStatus(200);
+
         $attributes = [
             'title'=>$this->faker->sentence,
             'description'=>$this->faker->paragraph,
@@ -28,6 +45,8 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_title()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['title'=>'']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
@@ -36,20 +55,32 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_description()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['description'=>'']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 
     /** @test */
-    public function a_user_can_view_a_project()
+    public function a_user_can_view_their_project()
     {
-        $this->withoutExceptionHandling();
+        $this->actingAs(factory('App\User')->create());
 
-        $project = factory('App\Project')->create();
+        $project = factory('App\Project')->create(['owner_id'=>auth()->id()]);
 
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_projects_of_other()
+    {
+        $this->actingAs(factory('App\User')->create());
+
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertStatus(403);
     }
 }
