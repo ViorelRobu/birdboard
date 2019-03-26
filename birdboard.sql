4d860104050dcdfd181d6051af0b13531b290105
Viorel Robu <wwjd.vio@gmail.com>
2019-03-26 21:02:49 +0200

    Latest DB export

--- birdboard.sql
+++ birdboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gazdă: 127.0.0.1
--- Timp de generare: mart. 10, 2019 la 09:14 PM
+-- Timp de generare: mart. 26, 2019 la 08:02 PM
 -- Versiune server: 10.1.37-MariaDB
 -- Versiune PHP: 7.3.0
 
@@ -25,6 +25,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structură tabel pentru tabel `activities`
+--
+
+CREATE TABLE `activities` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `project_id` bigint(20) UNSIGNED NOT NULL,
+  `subject_type` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `subject_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `description` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `changes` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Eliminarea datelor din tabel `activities`
+--
+
+INSERT INTO `activities` (`id`, `project_id`, `subject_type`, `subject_id`, `description`, `changes`, `created_at`, `updated_at`) VALUES
+(1, 10, NULL, NULL, 'created', '{\"before\":[],\"after\":{\"title\":\"Another new project\",\"description\":\"This is some random text\",\"owner_id\":1,\"updated_at\":\"2019-03-26 18:40:07\",\"created_at\":\"2019-03-26 18:40:07\",\"id\":10}}', '2019-03-26 16:40:07', '2019-03-26 16:40:07'),
+(2, 10, NULL, NULL, 'updated', '{\"before\":{\"title\":\"Another new project\"},\"after\":{\"title\":\"Another new project - update\",\"updated_at\":\"2019-03-26 18:44:45\"}}', '2019-03-26 16:44:45', '2019-03-26 16:44:45'),
+(3, 11, NULL, NULL, 'created', NULL, '2019-03-26 16:56:31', '2019-03-26 16:56:31'),
+(4, 11, NULL, NULL, 'updated', '{\"before\":{\"description\":\"Random blabber.\"},\"after\":{\"description\":\"Random blabber. And etc\",\"updated_at\":\"2019-03-26 18:57:01\"}}', '2019-03-26 16:57:01', '2019-03-26 16:57:01'),
+(5, 11, NULL, NULL, 'updated', '{\"before\":{\"title\":\"A new project has been create\"},\"after\":{\"title\":\"A new project has been create - change 2\"}}', '2019-03-26 16:58:57', '2019-03-26 16:58:57');
+
+-- --------------------------------------------------------
+
+--
 -- Structură tabel pentru tabel `migrations`
 --
 
@@ -39,10 +67,11 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(4, '2014_10_12_000000_create_users_table', 1),
-(5, '2014_10_12_100000_create_password_resets_table', 1),
-(6, '2019_03_06_123253_create_projects_table', 1),
-(8, '2019_03_09_071048_create_tasks_table', 2);
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_03_06_123253_create_projects_table', 1),
+(4, '2019_03_09_071048_create_tasks_table', 1),
+(9, '2019_03_22_164015_create_activities_table', 2);
 
 -- --------------------------------------------------------
 
@@ -67,6 +96,7 @@ CREATE TABLE `projects` (
   `owner_id` bigint(20) UNSIGNED NOT NULL,
   `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notes` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -75,24 +105,18 @@ CREATE TABLE `projects` (
 -- Eliminarea datelor din tabel `projects`
 --
 
-INSERT INTO `projects` (`id`, `owner_id`, `title`, `description`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Et in sunt voluptatem fugiat omnis delectus ex possimus.', 'Praesentium recusandae vitae vero voluptatem nihil. Nam alias aut ex labore.', '2019-03-06 16:59:59', '2019-03-06 16:59:59'),
-(2, 1, 'Et quia facere vitae ut molestiae dignissimos officia.', 'Sapiente sint dolorem ea quia. Ipsam quos excepturi et ipsa expedita praesentium sint. Autem perferendis voluptatem consectetur autem. Dolorem omnis et dignissimos odio veritatis.c;lsjdkofjksjdkfhjkgshdfjkgkjs dfkjgksldfjkg sdjlfgjksldfjg jsdklfjg kjsdklfgj ksdjfklgj klsdjfklgj lsdfj gj skldfjklglsdf ', '2019-03-06 17:00:29', '2019-03-06 17:00:29'),
-(3, 2, 'Rem eos sunt tempore et fuga et nisi.', 'Harum ad architecto voluptate. Aut aut saepe possimus quis explicabo. Facere tenetur maxime sit dignissimos ipsa odit omnis.', '2019-03-06 17:00:30', '2019-03-06 17:00:30'),
-(4, 5, 'Libero dicta similique corrupti ad animi quisquam.', 'Eligendi in omnis aut id molestiae eaque. Est reprehenderit culpa rerum ea. Omnis quis aut dolores animi et vitae. Quo id eligendi est eum voluptate. Ex sint dicta hic est quo voluptates qui iste.', '2019-03-06 17:00:31', '2019-03-06 17:00:31'),
-(5, 6, 'Et rem perferendis veritatis sunt quia.', 'Dolore laudantium ut ipsa maiores natus in maiores ullam. Quos suscipit odit dolor distinctio est. Ratione aut occaecati aut. Saepe incidunt est hic.', '2019-03-06 17:00:31', '2019-03-06 17:00:31'),
-(6, 7, 'Sunt officiis exercitationem quod repellendus.', 'Provident pariatur quod eveniet atque reprehenderit similique. Est molestiae voluptatem dignissimos aut. Voluptas quis provident magnam mollitia cum sint. Quam magnam sit voluptas eius ducimus explicabo.', '2019-03-06 17:00:32', '2019-03-06 17:00:32'),
-(8, 1, 'Dummy project title', 'Here I go again', '2019-03-08 15:05:56', '2019-03-08 15:05:56'),
-(9, 1, 'Testing and testing once again', 'Test the test of the test and then test it once more.', '2019-03-08 17:50:35', '2019-03-08 17:50:35'),
-(10, 9, 'Inventore alias impedit illum incidunt et ad.', 'Quae sit est ipsam voluptatem id nisi ut. Veniam voluptatum ut sed odit. Quas suscipit et molestiae est. Rerum voluptatem qui tenetur aut nesciunt.', '2019-03-09 13:08:46', '2019-03-09 13:08:46'),
-(11, 10, 'Itaque eius voluptate accusamus.', 'Mollitia animi sapiente eligendi aut labore illo perferendis rerum. Consequatur qui et molestiae sed quasi occaecati quo. Est est aut quaerat dolore tenetur.', '2019-03-09 13:15:02', '2019-03-09 13:15:02'),
-(12, 1, 'Nemo vel provident blanditiis maxime.', 'Labore impedit beatae dolorem corrupti. Velit ipsum maiores id ut. Quam et voluptatem dignissimos exercitationem. In voluptas a ea dolorem cumque ut fugit non.', '2019-03-09 13:25:38', '2019-03-09 13:25:38'),
-(13, 1, 'Repellat illum aut eligendi explicabo qui praesentium non.', 'Voluptas provident ea voluptas sit odit sunt. Natus dignissimos ut consectetur aut aliquid.', '2019-03-09 13:33:19', '2019-03-09 13:33:19'),
-(14, 1, 'Pray for wisdom', 'There is no better thing than the wisdom coming from God', '2019-03-09 17:22:47', '2019-03-09 17:22:47'),
-(15, 1, 'Go to Church', 'Tomorrow is Sunday and I need to go to the church.', '2019-03-09 17:24:29', '2019-03-09 17:24:29'),
-(16, 14, 'Sit ratione voluptatem quidem a ducimus cumque ut.', 'Amet quis tenetur sit non. Blanditiis et odit officia ut.', '2019-03-09 18:18:40', '2019-03-09 18:18:40'),
-(17, 15, 'Quia eos molestias quia eos voluptate placeat.', 'Ad architecto aliquid consequuntur quo. Fugit quis aut animi. Ut est dicta ut. Quasi consequatur ut maiores nulla quia. Ut qui minus et velit.', '2019-03-09 18:22:27', '2019-03-09 18:22:27'),
-(18, 16, 'Eius rerum recusandae officia nesciunt ut.', 'Sit iure sint non corrupti. Sit eaque voluptate nostrum adipisci quo quia. Doloremque eaque earum ut veniam ut a.', '2019-03-09 18:24:32', '2019-03-09 18:24:32');
+INSERT INTO `projects` (`id`, `owner_id`, `title`, `description`, `notes`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Give it a shot', 'Test 123', 'Test123\r\nanother test\r\n456\r\n111', '2019-03-12 18:07:52', '2019-03-26 03:28:49'),
+(2, 1, 'Give it a shot', 'Test. Changed.', NULL, '2019-03-19 16:43:45', '2019-03-19 16:43:54'),
+(3, 2, 'Provident nostrum et dolores dolor eos ab.', 'Nihil vero sed fugit nobis ipsam explicabo.', NULL, '2019-03-22 15:24:30', '2019-03-22 15:24:30'),
+(4, 3, 'Doloremque sapiente sed repellat aut.', 'Accusantium aperiam accusamus quia molestiae.', NULL, '2019-03-22 15:32:41', '2019-03-22 15:32:41'),
+(5, 4, 'changed', 'Dignissimos et aut error eveniet non excepturi est.', NULL, '2019-03-22 15:37:51', '2019-03-22 15:39:04'),
+(6, 5, 'changed', 'Repellendus minus ratione ea ut provident non.', NULL, '2019-03-22 15:43:58', '2019-03-22 15:45:26'),
+(7, 6, 'changed', 'Quo praesentium voluptatum est saepe sed magnam sed.', NULL, '2019-03-22 17:37:05', '2019-03-22 17:39:43'),
+(8, 1, 'Become a web developer', 'Learning and creating things in order to become a web developer.', NULL, '2019-03-26 14:03:24', '2019-03-26 14:04:03'),
+(9, 1, 'Test project', 'This is a test project', 'Adding some notes', '2019-03-26 14:28:42', '2019-03-26 14:40:48'),
+(10, 1, 'Another new project - update', 'This is some random text', NULL, '2019-03-26 16:40:07', '2019-03-26 16:44:45'),
+(11, 1, 'A new project has been create - change 2', 'Random blabber. And etc', NULL, '2019-03-26 16:56:31', '2019-03-26 16:58:57');
 
 -- --------------------------------------------------------
 
@@ -114,9 +138,14 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `project_id`, `body`, `completed`, `created_at`, `updated_at`) VALUES
-(1, 13, 'Adding a new task - updated', 1, '2019-03-10 04:31:18', '2019-03-10 04:34:25'),
-(2, 13, 'Testing the update method for tasks', 1, '2019-03-10 04:34:44', '2019-03-10 18:07:58'),
-(3, 13, 'Me adding a new task - updated', 1, '2019-03-10 18:07:48', '2019-03-10 18:07:55');
+(1, 1, 'The first task. Updated', 1, '2019-03-12 18:08:02', '2019-03-23 08:27:57'),
+(2, 1, 'The second task', 0, '2019-03-19 16:37:32', '2019-03-23 08:28:19'),
+(3, 7, 'do something', 1, '2019-03-22 17:38:39', '2019-03-22 17:39:43'),
+(4, 1, 'The third task', 0, '2019-03-23 08:28:05', '2019-03-23 08:28:05'),
+(5, 1, 'Last new task 3', 0, '2019-03-23 09:03:50', '2019-03-26 03:28:40'),
+(6, 8, 'Take online courses', 1, '2019-03-26 14:03:46', '2019-03-26 14:04:03'),
+(7, 8, 'Finish Laracasts videos', 0, '2019-03-26 14:03:55', '2019-03-26 14:03:55'),
+(8, 9, 'A new task has arrived', 1, '2019-03-26 14:28:51', '2019-03-26 14:39:53');
 
 -- --------------------------------------------------------
 
@@ -140,26 +169,24 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Viorel', 'viorel.robu@schweighofer.ro', NULL, '$2y$10$ZOZp90g1qXNZCeXzMv7r8uWTvD07qRWABtzQpLb8F.qieU594k0a.', NULL, '2019-03-06 16:58:51', '2019-03-06 16:58:51'),
-(2, 'Shaun Beatty III', 'epaucek@example.net', '2019-03-06 16:59:59', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'DXIYAKl70Y', '2019-03-06 16:59:59', '2019-03-06 16:59:59'),
-(3, 'Arch Block Sr.', 'jadon09@example.net', '2019-03-06 17:00:29', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'yTICuRqLSM', '2019-03-06 17:00:29', '2019-03-06 17:00:29'),
-(4, 'Gerda Runolfsdottir', 'arielle61@example.org', '2019-03-06 17:00:30', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'nhlWSOXqP3', '2019-03-06 17:00:30', '2019-03-06 17:00:30'),
-(5, 'Lisa Beer Jr.', 'btorp@example.org', '2019-03-06 17:00:31', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'hP6Q2YOv6S', '2019-03-06 17:00:31', '2019-03-06 17:00:31'),
-(6, 'Vickie Upton', 'tgoldner@example.com', '2019-03-06 17:00:31', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'dQHTISTYX8', '2019-03-06 17:00:31', '2019-03-06 17:00:31'),
-(7, 'Lucie Altenwerth', 'david90@example.com', '2019-03-06 17:00:32', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '2LTCWTAp9b', '2019-03-06 17:00:32', '2019-03-06 17:00:32'),
-(8, 'Geo Reichert Sr.', 'hirthe.vance@example.com', '2019-03-06 17:00:32', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'sEhHY0Jkmr', '2019-03-06 17:00:32', '2019-03-06 17:00:32'),
-(9, 'Gaetano Osinski', 'mitchell.claire@example.org', '2019-03-09 13:08:46', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'FR7e937o1Z', '2019-03-09 13:08:46', '2019-03-09 13:08:46'),
-(10, 'Mr. Mark Treutel', 'oscar78@example.net', '2019-03-09 13:15:02', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'wJ1CyCLXxy', '2019-03-09 13:15:02', '2019-03-09 13:15:02'),
-(11, 'Mrs. Bulah Leannon Jr.', 'raheem34@example.org', '2019-03-09 13:25:38', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'FBKUByGNMf', '2019-03-09 13:25:38', '2019-03-09 13:25:38'),
-(12, 'Keshawn Murray', 'marcellus.schuster@example.com', '2019-03-09 13:33:19', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'f68phMLKgQ', '2019-03-09 13:33:19', '2019-03-09 13:33:19'),
-(13, 'Haskell Ferry', 'jazlyn.weimann@example.org', '2019-03-09 17:43:28', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '2mVun9w8o2', '2019-03-09 17:43:28', '2019-03-09 17:43:28'),
-(14, 'Ida Little', 'carrie55@example.net', '2019-03-09 18:18:40', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'aK2U51lmxx', '2019-03-09 18:18:40', '2019-03-09 18:18:40'),
-(15, 'Chesley Baumbach Jr.', 'hmoore@example.com', '2019-03-09 18:22:27', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'ebtwApTqP1', '2019-03-09 18:22:27', '2019-03-09 18:22:27'),
-(16, 'Harley Nitzsche', 'jazmyn16@example.net', '2019-03-09 18:24:32', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '3BCABeEgzU', '2019-03-09 18:24:32', '2019-03-09 18:24:32');
+(1, 'Viorel', 'viorel.robu@schweighofer.ro', NULL, '$2y$10$3g0K0W6aCLyRMB5t.09NQOIrPGFYn2513hBFCtCOdDoE61tSbRiuK', NULL, '2019-03-12 18:07:08', '2019-03-12 18:07:08'),
+(2, 'Prof. Soledad Stroman III', 'nicolas.niko@example.com', '2019-03-22 15:24:30', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Dj6LHoEl2C', '2019-03-22 15:24:30', '2019-03-22 15:24:30'),
+(3, 'Prof. Kieran Mohr DDS', 'zola14@example.net', '2019-03-22 15:32:41', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'xdsOtFBVG9', '2019-03-22 15:32:41', '2019-03-22 15:32:41'),
+(4, 'Prof. Vesta Lind', 'virginie83@example.net', '2019-03-22 15:37:51', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '3KAUQuSRJo', '2019-03-22 15:37:51', '2019-03-22 15:37:51'),
+(5, 'Hilton Jaskolski Jr.', 'romaguera.edwardo@example.com', '2019-03-22 15:43:58', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', '9XtzNutQeO', '2019-03-22 15:43:58', '2019-03-22 15:43:58'),
+(6, 'Garfield Abbott', 'borer.creola@example.net', '2019-03-22 17:37:05', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'TlZBl3uGFl', '2019-03-22 17:37:05', '2019-03-22 17:37:05');
 
 --
 -- Indexuri pentru tabele eliminate
 --
+
+--
+-- Indexuri pentru tabele `activities`
+--
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `activities_subject_type_subject_id_index` (`subject_type`,`subject_id`),
+  ADD KEY `activities_project_id_foreign` (`project_id`);
 
 --
 -- Indexuri pentru tabele `migrations`
@@ -198,32 +225,44 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pentru tabele `activities`
+--
+ALTER TABLE `activities`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT pentru tabele `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pentru tabele `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pentru tabele `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pentru tabele `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constrângeri pentru tabele eliminate
 --
+
+--
+-- Constrângeri pentru tabele `activities`
+--
+ALTER TABLE `activities`
+  ADD CONSTRAINT `activities_project_id_foreign` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE;
 
 --
 -- Constrângeri pentru tabele `projects`
